f2411f2e81415135bd4b93d15c2d029d41bfd0e0
Saxon Hunt <shunt@twilio.com>
2022-11-03 00:17:47 -0700

    use anyhow

--- rust/src/environment.rs
+++ rust/src/environment.rs
@@ -1,5 +1,5 @@
+use anyhow::{Context, Result};
 use std::str::FromStr;
-
 use strum::EnumString;
 
 pub const SECRET_PREFIX: &str = "SECRET_";
@@ -13,14 +13,28 @@ pub enum Environments {
 }
 
 ///  Get file path from env var & return file contents as a string.
-pub fn read_file_from_env_path(env_secret_name: &str) -> Result<_, _> {
+pub fn read_file_from_env_path(env_secret_name: &str) -> Result<String> {
     let file_name = std::env::var(env_secret_name)?;
-    std::fs::read_to_string(file_name)
+    std::fs::read_to_string(&file_name).with_context(|| {
+        format!(
+            "failed to read file_path {} found at env var {}",
+            file_name, env_secret_name
+        )
+    })
 }
 
 /// Read env var `ENVIRONMNENT`, raises an error if not set or not valid environment
-pub fn get_deployment_env() -> Result<Environments, _> {
+pub fn get_deployment_env() -> Result<Environments> {
     let env = std::env::var("ENVIRONMENT")?;
+    Environments::from_str(&env).with_context(|| {
+        format!(
+            "`ENVIRONMENTS` variable is not a valid environment: {}",
+            env
+        )
+    })
+}
 
-    Environments::from_str(&env)
+pub fn get_env_var(env_var_name: &str) -> Result<String> {
+    std::env::var(env_var_name)
+        .with_context(|| format!("Could not read environment variable: `{}`", env_var_name))
 }
